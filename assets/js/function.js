074708f80510ed630b714b4b83ff953dda508560
ema.ferr <78190534+emaferr@users.noreply.github.com>
2021-05-03 21:00:47 +0200

    Sistemato layout

--- assets/js/function.js
+++ assets/js/function.js
@@ -5,17 +5,31 @@ function cancellaDati() {
 function generaBiglietto() {
     // Nome passeggero
     var nomeUtente = document.getElementById("nome_utente").value;
+
     var chilometri = document.getElementById("chilometri_percorso").value;
+
+    if ( chilometri < 10 || chilometri > 1500 ) {
+        alert("Il tragitto percorso dai nostri treni va da 5 a 1550 chilometri")
+    } else if (isNaN (chilometri) ) {
+        alert("Il valore chilometri inserito non è corretto. Inserire un numero compreso fra 5 e 1500")   
+    } else {
+        
+    }
+
+    // Categoria selezionata dall'utente
     var categoriaUtente = document.getElementById("exampleFormControlSelect1").value;
+    
     // Prezzo calcolato
     var prezzoBiglietto = chilometri * 0.21;
 
     if (categoriaUtente == "Minorenne") {
         prezzoBiglietto = (prezzoBiglietto - (prezzoBiglietto * 20) / 100);
+        document.getElementById("offerta").innerHTML = "Sconto Minorenne"
     }else if (categoriaUtente == "Over 65 anni"){
         prezzoBiglietto = (prezzoBiglietto - (prezzoBiglietto * 40) / 100);
+        document.getElementById("offerta").innerHTML = "Sconto Over 65 anni"
     }else{
-
+        document.getElementById("offerta").innerHTML = "Nessuna offerta applicabile"
     }
 
     document.getElementById("nome_biglietto").innerHTML = nomeUtente;
--- assets/js/main.js
+++ assets/js/main.js
@@ -1,9 +1,5 @@
 // Creiamo un finto biglietto del treno con:
 
-// Categoria selezionata dall'utente
-
-
-
 // Codice treno (numero casuale tra 90000 e 100000 escluso)
 var codiceTreno = Math.floor(Math.random() * (100000 - 90000) + 90000);
 
